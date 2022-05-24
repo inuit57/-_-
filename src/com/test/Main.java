f17bc7ae47f521067804d4e29991cda3f7eacd5c
inuit57 <inuit57@naver.com>
2022-05-24 15:57:36 +0900

    feat : 가장 짧은 문자거리

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -5,25 +5,46 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String solution(String str){
-        String answer = "";
-        ArrayList<Character> charArr = new ArrayList<>();
+    public static int[] solution(String str,char ch){
 
-        for(char ch : str.toCharArray()){
-            if ( charArr.indexOf(ch) < 0 || charArr.isEmpty()){
-                charArr.add(ch);
-                answer += ch ;
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int idx = 0;
+        idx = str.indexOf(ch);
+        for(int i =0 ; i < idx ; i++){
+            answer.add(idx - i);
+        }
+        answer.add(0);
+        str = str.substring(idx+1);
+
+        while( (idx = str.indexOf(ch)) >= 0){
+            for(int i =0 ; i < idx ; i++){
+                answer.add( Integer.min(i+1, idx - i));
+            }
+            answer.add(0);
+            str = str.substring(idx+1);
+        }
+
+        if( !str.isEmpty()){
+            for(int i = 1 ; i<= str.length() ; i++){
+                answer.add(i);
             }
         }
 
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
 
         String inputStr = in.next();
-        String solution = solution(inputStr);
-        System.out.println(solution);
+        char ch = in.next().charAt(0);
+
+        int[] arr01 = solution(inputStr,ch);
+        for (int i : arr01) {
+            System.out.print(i+" ");
+        }
     }
 }
+
+
