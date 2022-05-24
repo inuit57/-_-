e833dc359c148b8a0e7c3ae47ed96b48a1823bcc
inuit57 <inuit57@naver.com>
2022-05-24 14:59:05 +0900

    feat : 중복문자 제거

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,23 +1,21 @@
 package com.test;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static String solution(String strArr){
-
-        String alphaOnly = new StringBuilder(strArr.replaceAll("[^a-zA-Z]","")).reverse().toString();
-
+    public static String solution(String str){
         String answer = "";
-        int idx = 0 ;
-        for(char ch : strArr.toCharArray()){
-            if( Character.isAlphabetic(ch)){
-                answer += alphaOnly.charAt(idx);
-                idx++;
-            }else {
-                answer += ch;
+        ArrayList<Character> charArr = new ArrayList<>();
+
+        for(char ch : str.toCharArray()){
+            if ( charArr.indexOf(ch) < -1 || charArr.isEmpty()){
+                charArr.add(ch);
+                answer += ch ;
             }
         }
+
         return answer;
     }
 
@@ -25,7 +23,7 @@ public class Main {
         Scanner in=new Scanner(System.in);
 
         String inputStr = in.next();
-
-        System.out.println(solution(inputStr));
+        String solution = solution(inputStr);
+        System.out.println(solution);
     }
 }
