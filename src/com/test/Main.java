f460f87532b1e7ce26a28c49391c2d8eb079506a
inuit57 <inuit57@naver.com>
2022-05-25 13:31:43 +0900

    feat: 가위바위보

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int solution(Scanner in , int size){
-        int cnt = 1 ;
-        int curr = in.nextInt();
-        for(int i =1 ; i < size  ; i++){
-            int next = in.nextInt();
-            if( next > curr){
-                curr = next ;
-                cnt ++ ;
-            }
+    public static void solution(int[][] arr , int size){
+        final String result = "ABDAB" ;
+        for(int i = 0 ; i< size; i++){
+            System.out.println(result.charAt(2+(arr[0][i]-arr[1][i])));
         }
-        return cnt ;
     }
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
 
         int n = in.nextInt();
-        int result = solution(in, n);
-        System.out.println(result);
+        int[][] arr = new int[2][n];
+
+        for(int i = 0 ; i < 2; i++){
+            for(int j = 0 ; j< n ; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        solution(arr,n);
+
 
     }
 }
