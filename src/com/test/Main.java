77240cb03390b3d4c3b1227a1fcb46bbf45f1a23
inuit57 <inuit57@naver.com>
2022-05-26 14:41:48 +0900

    feat: 격자판 최대합

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -8,34 +8,48 @@ public class Main {
         Scanner in=new Scanner(System.in);
 
         int n = in.nextInt();
-        int[] arr = new int[n] ;
+        int[][] arr = new int[n][n] ;
 
         for(int i = 0 ; i < n ; i++){
-            arr[i] = in.nextInt();
-        }
-
-        int[] result = solution(arr);
-        for(int i =0 ; i< n ; i++){
-            System.out.print(result[i] + " ");
-        }
-
-    }
-
-    private static int[] solution(int[] arr) {
-        int size = arr.length;
-        int[] rank = new int[size] ;
-        for(int i = 0 ; i < size ; i++){
-            rank[i]++;
-            for(int j = i+1 ; j < size ; j++){
-                if( arr[j] > arr[i]){
-                    rank[i]++;
-                }else if(arr[j] < arr[i]){
-                    rank[j]++;
-                }
+            for(int j= 0 ; j< n ; j++) {
+                arr[i][j] = in.nextInt();
             }
         }
-        return rank;
+
+        System.out.println(solution(arr));
+
     }
+
+    private static int solution(int[][] arr) {
+        int max = -1;
+        int size = arr[0].length;
+        for (int i = 0; i < size; i++) {
+            int sum0 = 0;
+            int sum1 = 0;
+            for (int j = 0; j < size; j++) {
+                sum0 += arr[i][j]; // 가로줄 합
+                sum1 += arr[j][i]; // 세로줄 합
+            }
+            if (max < Math.max(sum0, sum1)) {
+                max = Math.max(sum0, sum1);
+            }
+        }
+
+        int sum = 0;
+        int sum2 = 0 ;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i][i]; // 대각선
+            sum2 += arr[size-i-1][size-i-1] ;
+        }
+        if (max < Math.max(sum,sum2)) {
+            max = Math.max(sum,sum2);
+        }
+
+
+        return  max;
+    }
+
+
 
 }
 
