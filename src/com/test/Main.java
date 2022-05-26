57b11df0109ae96f7569b12dcc5eb4a34a0c7bac
inuit57 <inuit57@naver.com>
2022-05-26 21:51:54 +0900

    feat: 봉우리

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[][] arr ;
+    static int n ;
+
+    static int[] px = {1,-1,0,0};
+    static int[] py = {0,0,1,-1};
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
 
-        int n = in.nextInt();
-        int[][] arr = new int[n][n] ;
+        n = in.nextInt();
+        arr = new int[n][n] ;
 
         for(int i = 0 ; i < n ; i++){
             for(int j= 0 ; j< n ; j++) {
@@ -16,37 +22,35 @@ public class Main {
             }
         }
 
-        System.out.println(solution(arr));
+        System.out.println(solution());
 
     }
 
-    private static int solution(int[][] arr) {
-        int max = -1;
-        int size = arr[0].length;
-        for (int i = 0; i < size; i++) {
-            int sum0 = 0;
-            int sum1 = 0;
-            for (int j = 0; j < size; j++) {
-                sum0 += arr[i][j]; // 가로줄 합
-                sum1 += arr[j][i]; // 세로줄 합
+
+
+    private static int topCheck(int x, int y , int curr){
+
+        if( x < 0 || y < 0 || x >= n || y >= n ){
+            return 1;
+        }
+        if( arr[x][y] >= curr) return  0;
+        return 1;
+    }
+
+    private static int solution() {
+        int cnt = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j =0 ; j< n ; j++){
+                int isTop = 0;
+                for(int k=0; k < 4; k++){
+                    isTop += topCheck(i+px[k], j+py[k],arr[i][j]);
+                }
+                if(isTop == 4){
+                    cnt ++;
+                }
             }
-            if (max < Math.max(sum0, sum1)) {
-                max = Math.max(sum0, sum1);
-            }
         }
-
-        int sum = 0;
-        int sum2 = 0 ;
-        for (int i = 0; i < size; i++) {
-            sum += arr[i][i]; // 대각선
-            sum2 += arr[size-i-1][size-i-1] ;
-        }
-        if (max < Math.max(sum,sum2)) {
-            max = Math.max(sum,sum2);
-        }
-
-
-        return  max;
+        return cnt;
     }
 
 
