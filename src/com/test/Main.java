927847e88b4ab1cb2a910f859f95b727163a8753
inuit57 <inuit57@naver.com>
2022-05-26 13:34:32 +0900

    feat: 뒤집은 소수

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,32 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void solution(int[][] arr , int size){
-
+    static int[] arr = new int[100001] ;
+    public static void makePrimeArr(int n){
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2 ; i <= Math.sqrt((double)n) ; i++){
+            int chk = i+i;
+            while(chk <= n){
+                arr[chk] = 1;
+                chk += i ;
+            }
+        }
     }
 
-    static int[] arr = new int[45];
-
-    public static int fibo(int n){
-
-        if( n <= 1){
-            arr[n] = 1;
-            return n;
-        }
-        if( arr[n] != 0) return arr[n] ;
-
-        return arr[n] = fibo(n-1) + fibo(n-2) ;
+    public static boolean isPrime(int n){
+        return (arr[n]==0) ;
     }
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        makePrimeArr(100000);
 
         int n = in.nextInt();
+        for(int i = 0 ; i< n; i++){
+            String num = in.next();
+            String tmp = new StringBuilder(num).reverse().toString();
+            int numI = Integer.valueOf(tmp);
 
-        fibo(n);
-        for(int i =1 ; i <= n ; i++){
-            System.out.print(arr[i]+" ");
+            if(isPrime(numI)){
+                System.out.print(numI+" ");
+            }
         }
 
     }
