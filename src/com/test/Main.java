06cf5677e5eabff25e5ccd41ca8073176fff2b7b
inuit57 <inuit57@naver.com>
2022-05-26 14:18:20 +0900

    feat: 점수 계산

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,41 +1,30 @@
 package com.test;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static int[] arr = new int[100001] ;
-    public static void makePrimeArr(int n){
-        arr[0] = 1;
-        arr[1] = 1;
-        for(int i = 2 ; i <= Math.sqrt((double)n) ; i++){
-            int chk = i+i;
-            while(chk <= n){
-                arr[chk] = 1;
-                chk += i ;
-            }
-        }
-    }
-
-    public static boolean isPrime(int n){
-        return (arr[n]==0) ;
-    }
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        makePrimeArr(100000);
 
         int n = in.nextInt();
-        for(int i = 0 ; i< n; i++){
-            String num = in.next();
-            String tmp = new StringBuilder(num).reverse().toString();
-            int numI = Integer.valueOf(tmp);
+        in.nextLine();
+        String inputStr = in.nextLine().replace(" ","");
 
-            if(isPrime(numI)){
-                System.out.print(numI+" ");
+        System.out.println(solution(inputStr));
+    }
+
+    private static int solution(String inputStr) {
+        int score = 0 ;
+        String[] results = inputStr.split("0");
+
+        for(String str : results){
+            for(int i = 1 ; i <= str.length(); i++){
+                score += i ;
             }
         }
 
+        return score;
     }
 }
 
