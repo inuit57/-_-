157938aac1cb05af3b7a660e19c236eca7e17f18
inuit57 <inuit57@naver.com>
2022-05-27 10:42:30 +0900

    feat: 멘토링

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,57 +4,51 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[][] arr ;
-    static int n ;
-
-    static int[] px = {1,-1,0,0};
-    static int[] py = {0,0,1,-1};
+    static int[][] arr;
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int stu, ex;
+        stu = in.nextInt(); // 4
+        ex = in.nextInt(); // 3
 
-        n = in.nextInt();
-        arr = new int[n][n] ;
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j= 0 ; j< n ; j++) {
+        arr = new int[ex][stu];
+        for (int i = 0; i < stu; i++) {
+            for (int j = 0; j < ex; j++) {
                 arr[i][j] = in.nextInt();
             }
         }
 
-        System.out.println(solution());
 
+        System.out.println("n = " + solution(stu, ex));
     }
 
 
-
-    private static int topCheck(int x, int y , int curr){
-
-        if( x < 0 || y < 0 || x >= n || y >= n ){
-            return 1;
-        }
-        if( arr[x][y] >= curr) return  0;
-        return 1;
-    }
-
-    private static int solution() {
-        int cnt = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j =0 ; j< n ; j++){
-                int isTop = 0;
-                for(int k=0; k < 4; k++){
-                    isTop += topCheck(i+px[k], j+py[k],arr[i][j]);
+    private static int solution(int stu, int ex) {
+        int answer = 0;
+        for (int i = 1; i <= stu; i++) {
+            for (int j = 1; j <= stu; j++) {
+                // 학생 조합 설정
+                int cnt = 0;
+                for (int k = 0; k < ex; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < stu; s++) {
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
+                    }
+                    if (pi < pj) cnt++;
                 }
-                if(isTop == 4){
-                    cnt ++;
+                if (cnt == ex) {
+                    answer++;
                 }
             }
+
         }
-        return cnt;
+        return answer;
     }
-
-
-
 }
+
+
+
 
 
