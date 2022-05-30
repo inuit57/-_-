de1283b227afc680bc7f796036f737feb1024a05
inuit57 <inuit57@naver.com>
2022-05-30 16:32:04 +0900

    feat : 아나그램

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,29 +3,32 @@ package com.test;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int n = in.nextInt(); // 전체 숫자
+        //int n = in.nextInt(); // 전체 숫자
 
         Map<Character,Integer> map = new HashMap<>();
 
-        String vote = in.next();
+        String input = in.next();
 
-        for(char x : vote.toCharArray()){
+        for(char x : input.toCharArray()){
             map.put(x, map.getOrDefault(x,0)+1);
         }
-        int max = Integer.MIN_VALUE;
-        char answer = ' ';
-        for (char key : map.keySet()){
-            // System.out.println(x+" "+ map.get(x));
-            if( max < map.get(key)){
-                max = map.get(key);
-                answer = key ;
+
+        String input2 = in.next();
+        Map<Character,Integer> map2 = new HashMap<>();
+        for(char x : input2.toCharArray()){
+            map2.put(x, map2.getOrDefault(x,0)+1);
+        }
+
+        for(char key : map.keySet()){
+            if( map.getOrDefault(key,0) !=
+                    map2.get(key)){
+                System.out.println("NO");
+                return;
             }
         }
-        System.out.println(answer);
-
+        System.out.println("YES");
     }
 
     private static int solution(int n, int k, int[] arr) {
