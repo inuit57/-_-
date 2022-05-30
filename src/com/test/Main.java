c73e15e3bf270e4f0d89884041bd8c685f3740c7
inuit57 <inuit57@naver.com>
2022-05-30 10:48:32 +0900

    feat : 연속부분 수열

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -8,29 +8,42 @@ public class Main {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt(); // 전체 숫자
 
-        int k = in.nextInt(); // 연속된 일 수
+        int m = in.nextInt(); // 목표값
 
         int[] arr = new int[n];
         for(int i=0 ; i< n; i++){
             arr[i] = in.nextInt();
         }
 
-        System.out.println(solution(n,k,arr) );
+        System.out.println(solution(n,m,arr) );
 
     }
 
-    private static int solution(int n, int k, int[] arr) {
+    private static int solution(int n, int m, int[] arr) {
 
-        int answer , sum =0 ;
-        // 첫번째 윈도우 생성
-        for(int i =0 ; i< k ; i++) sum +=arr[i] ;
-        answer = sum;
+        int lt, rt, cnt ,sum ;
+        lt = rt = cnt =  0  ;
+        sum = arr[rt] ;
 
-        for(int i =k ; i< n ; i++){
-            sum += (arr[i] - arr[i-k]);
-            answer = Math.max(answer,sum);
+        while( rt < n ){
+            if( sum < m){
+                rt ++;
+                if( rt >= n) break;
+                sum += arr[rt];
+            }else if( sum > m){
+                sum -= arr[lt] ;
+                lt ++;
+            }
+            if( sum == m){
+                cnt ++ ;
+                sum -= arr[lt] ;
+                lt ++;
+            }
+
         }
-        return answer ;
+
+
+        return cnt ;
     }
 
 
