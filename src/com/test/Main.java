e70f4ffed72dabc0c303ce21e25bf071c5b22c20
inuit57 <inuit57@naver.com>
2022-05-30 15:34:12 +0900

    feat : 최대 길이 연속부분 수열(강사님 풀이)

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -8,41 +8,30 @@ public class Main {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt(); // 전체 숫자
 
-//        int m = in.nextInt(); // 목표값
+        int k = in.nextInt(); // 바꿀 수 있는 0의 갯수
 
-//        int[] arr = new int[n];
-//        for(int i=0 ; i< n; i++){
-//            arr[i] = in.nextInt();
-//        }
+        int[] arr = new int[n];
+        for(int i=0 ; i< n; i++){
+            arr[i] = in.nextInt();
+        }
 
-        System.out.println(solution(n) );
+        System.out.println(solution(n,k,arr) );
 
     }
 
-    private static int solution(int n) {
+    private static int solution(int n, int k, int[] arr) {
 
-        int lt, cnt ,sum ;
-        cnt = sum =  0  ;
+        int lt, cnt ,answer ;
+        lt = cnt = answer = 0 ;
 
-        lt = 1;
-
-        // 강사님 방법은 rt를 for 문으로 돌리고
-        // 그 안에서 lt를 증가시키는 방법
-        // 내가 사용한 while 문의 경우 배열 밖으로 튀는 경우가 있었는데
-        // 이렇게 하면 그런 경우는 없지 싶다.
-        for(int rt = 1 ; rt <= (n/2)+1 ; rt++) {
-            sum += rt ;
-            if( sum == n ){
-                cnt ++ ;
+        for(int rt = 0 ; rt < n  ; rt++) {
+            if( arr[rt] == 0) cnt ++;
+            while (cnt > k){
+                if( arr[lt] == 0) cnt --;
+                lt ++;
             }
-            // 하나만 빼선 안될 수 있으니까 while 문을 사용한다.
-            while ( sum >= n){
-                sum -= lt;
-                lt++;
-                if( sum == n) cnt++;
-                // 빼고 나서도 변경되니까 비교를 해줘야한다.
-            }
+            answer = Math.max(answer, (rt - lt +1)) ;
         }
-        return cnt ;
+        return answer ;
     }
 }
