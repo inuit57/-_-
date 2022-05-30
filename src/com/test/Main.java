6d99176843c2583da0adf2c932f6367a6b41ece7
inuit57 <inuit57@naver.com>
2022-05-30 10:54:57 +0900

    feat : 연속부분 수열(강사님 풀이)

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -21,35 +21,25 @@ public class Main {
 
     private static int solution(int n, int m, int[] arr) {
 
-        int lt, rt, cnt ,sum ;
-        lt = rt = cnt =  0  ;
-        sum = arr[rt] ;
+        int lt, cnt ,sum ;
+        lt = cnt = sum =  0  ;
 
-        while( rt < n ){
-            if( sum < m){
-                rt ++;
-                if( rt >= n) break;
-                sum += arr[rt];
-            }else if( sum > m){
-                sum -= arr[lt] ;
-                lt ++;
-            }
-            if( sum == m){
+        // 강사님 방법은 rt를 for 문으로 돌리고
+        // 그 안에서 lt를 증가시키는 방법
+        // 내가 사용한 while 문의 경우 배열 밖으로 튀는 경우가 있었는데
+        // 이렇게 하면 그런 경우는 없지 싶다.
+        for(int rt = 0 ; rt < n ; rt++) {
+            sum += arr[rt] ;
+            if( sum == m ){
                 cnt ++ ;
-                sum -= arr[lt] ;
-                lt ++;
             }
-
+            // 하나만 빼선 안될 수 있으니까 while 문을 사용한다.
+            while ( sum >= m){
+                sum -= arr[lt++];
+                if( sum == m) cnt++;
+                // 빼고 나서도 변경되니까 비교를 해줘야한다.
+            }
         }
-
-
         return cnt ;
     }
-
-
 }
-
-
-
-
-
