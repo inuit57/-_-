3678b5e21bf7a4d38c54ce453e600676c95ca575
inuit57 <inuit57@naver.com>
2022-05-31 15:37:12 +0900

    feat : 올바른 괄호(stack)

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,85 +3,37 @@ package com.test;
 import com.sun.source.tree.Tree;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
-    static TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
 
     public static void main(String[] args){
         Main T = new Main();
         Scanner in=new Scanner(System.in);
-        int n = in.nextInt(); //
-        int k = in.nextInt(); // 연속된 길이
 
-        int[] arr = new int[n];
-        for(int i =0 ; i < n ; i++){
-            arr[i] = in.nextInt();
-        }
+        String str = in.next();  // 괄호 문자열
 
-        boolean[] chosen = new boolean[n];
-        combination_DFS(arr, 0, n,k ,0 );
-
-        System.out.println(treeSet.stream().collect(Collectors.toList()).get(k-1));
-
-//        combination_BFS(arr,chosen, 0, n,k ,0 );
-//
-//        System.out.println(treeSet.stream().collect(Collectors.toList()).get(k-1));
-        //System.out.println(T.solution(n,k,arr));
+        System.out.println(T.solution(str));
     }
 
-    // DFS 방식 조합 탐색
-    static void combination_DFS(int[] arr,  int start ,int n,  int r, int sum ){
-        if( r== 0){
-            treeSet.add(sum);
-            return;
-        }
-        if( start == n){
-            return;
-        }
-        // start 번째를 선택한 경우
-        combination_DFS(arr,  start+1, n, r-1, sum+arr[start]);
+    private String solution(String str) {
+        Stack<Character> stack = new Stack<>();
 
-        // start 번째를 선택하지 않은 경우
-        combination_DFS(arr,  start+1, n, r, sum);
-    }
-
-    static void combination_BFS(int[] arr, boolean[] chosen , int start ,int n,  int r, int sum ){
-        if( r == 0){
-            treeSet.add(sum);
-            return;
-        }
-        for(int i =start ; i < n ; i++){
-            //chosen[i] = true;
-            combination_BFS(arr,chosen, start, n, r-1, sum+arr[start]);
-            //chosen[i] = false;
-            combination_BFS(arr,chosen, start, n, r, sum);
-        }
-    }
-
-
-    private int solution(int n, int k, int[] arr) {
-        // 조합 공식 : n!/(n-3)!*3!
-        int max_no = (n*(n-1)*(n-2))/6;
-        if( k > max_no) return -1;
-
-        int answer = 0 ;
-        // 저장될 때 내림차순으로 저장된다.
-        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
-
-        // 무식하게 3중 포문으로 3개 모두 뽑아내자.
-        // 3개 뽑는 데에는 이게 가장 효율적인 방법일까?
-        for(int i =0 ; i < n ; i++){
-            for(int j= i+1 ; j<n; j++){
-                for(int l = j+1 ; l< n ;l++){
-                    Tset.add(arr[i]+arr[j]+arr[l]);
+        for(char x : str.toCharArray()){
+            if( x == ')'){
+                if(stack.isEmpty()){
+                    return "NO";
                 }
+                stack.pop();
+            }else{
+                stack.push(x); // '('
             }
         }
-
-        answer = Tset.stream().collect(Collectors.toList()).get(k-1);
-        return answer;
+        if( !stack.isEmpty() ){
+            return "NO";
+        }
+        return "YES" ;
     }
+
 
 }
