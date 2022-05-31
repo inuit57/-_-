d5bb72885522cf367d1ddb33ea2cb467b8c82b69
inuit57 <inuit57@naver.com>
2022-05-31 13:50:39 +0900

    feat : k번째 큰수(DFS 방식)

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,14 +1,18 @@
 package com.test;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    static TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+
     public static void main(String[] args){
         Main T = new Main();
         Scanner in=new Scanner(System.in);
         int n = in.nextInt(); //
-
         int k = in.nextInt(); // 연속된 길이
 
         int[] arr = new int[n];
@@ -16,8 +20,31 @@ public class Main {
             arr[i] = in.nextInt();
         }
 
-        System.out.println(T.solution(n,k,arr));
+        boolean[] chosen = new boolean[n];
+        combination_DFS(arr,chosen, 0, n,k ,0 );
+
+        System.out.println(treeSet.stream().collect(Collectors.toList()).get(k-1));
+        //System.out.println(T.solution(n,k,arr));
     }
+
+    // DFS 방식 조합 탐색
+    static void combination_DFS(int[] arr, boolean[] chosen , int start ,int n,  int r, int sum ){
+        if( r== 0){
+            treeSet.add(sum);
+            return;
+        }
+        if( start == n){
+            return;
+        }
+        // start 번째를 선택한 경우
+        chosen[start] = true;
+        combination_DFS(arr, chosen, start+1, n, r-1, sum+arr[start]);
+
+        // start 번째를 선택하지 않은 경우
+        chosen[start] = false;
+        combination_DFS(arr, chosen, start+1, n, r, sum);
+    }
+
 
     private int solution(int n, int k, int[] arr) {
         // 조합 공식 : n!/(n-3)!*3!
