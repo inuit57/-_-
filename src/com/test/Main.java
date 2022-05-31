36d91e9e251c8ac43b09eeee460ab7fd17281328
inuit57 <inuit57@naver.com>
2022-05-31 09:59:55 +0900

    feat : 매출액의 종류

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -5,40 +5,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        //int n = in.nextInt(); // 전체 숫자
+        int n = in.nextInt(); //
 
-        Map<Character,Integer> map = new HashMap<>();
+        int k = in.nextInt(); // 연속된 길이
 
-        String input = in.next();
-
-        for(char x : input.toCharArray()){
-            map.put(x, map.getOrDefault(x,0)+1);
+        int[] arr = new int[n];
+        for(int i =0 ; i< n ; i++){
+            arr[i] = in.nextInt();
         }
-
-        String input2 = in.next();
-        for(char x : input2.toCharArray()){
-            if( !map.containsKey(x) || map.get(x) == 0){
-                System.out.println("NO");
-                return;
-            }
-            map.put(x, map.get(x)-1); // 하나씩 빼준다.
-        }
-        System.out.println("YES");
+        //System.out.println(solution(n,k,arr));
+        solution(n,k,arr);
     }
 
     private static int solution(int n, int k, int[] arr) {
+        int lt =0;
 
-        int lt, cnt ,answer ;
-        lt = cnt = answer = 0 ;
-
-        for(int rt = 0 ; rt < n  ; rt++) {
-            if( arr[rt] == 0) cnt ++;
-            while (cnt > k){
-                if( arr[lt] == 0) cnt --;
-                lt ++;
-            }
-            answer = Math.max(answer, (rt - lt +1)) ;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        // 최초로 값 집어넣기
+        for(int i =0 ; i < k ; i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        return answer ;
+        System.out.print(map.size()+" ");
+
+        for(int rt=k ; rt < n ;rt++){
+            map.put(arr[lt],
+                    map.get(arr[lt])-1);
+            if( map.get(arr[lt])== 0){
+                map.remove(arr[lt]);
+            }
+            lt++;
+            map.put(arr[rt],
+                    map.getOrDefault(arr[rt],0)+1);
+            System.out.print(map.size() + " ");
+        }
+        return 0;
     }
 }
