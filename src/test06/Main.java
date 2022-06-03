4f8365f4411823be5cb812bfc07737fbee84b3cc
inuit57 <inuit57@naver.com>
2022-06-03 14:58:43 +0900

    feat : 공주 구하기

---
+++ src/test06/Main.java
@@ -0,0 +1,37 @@
+package test06;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Main T = new Main();
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        System.out.println(T.solution(n,k));
+
+    }
+
+    private int solution(int n, int k) {
+
+        int[] arr = new int[n+1] ;
+
+        int remove = 0;
+        arr[0] = 1;
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < k; j++) {
+                remove = (remove + 1) % (n+1);
+                while (arr[remove] == 1) {
+                    remove = (remove + 1) % n;
+                }
+            }
+            //System.out.println("remove[" + i + "] = " + remove);
+            arr[remove] = 1;
+        }
+
+        return  remove ;
+    }
+}
