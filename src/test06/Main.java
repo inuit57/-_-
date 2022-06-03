27c7e35549375efb9b00d6df72372b978c1b6661
inuit57 <inuit57@naver.com>
2022-06-03 15:01:56 +0900

    feat : 공주 구하기(실패)

--- src/test06/Main.java
+++ src/test06/Main.java
@@ -21,15 +21,21 @@ public class Main {
 
         int remove = 0;
         arr[0] = 1;
-        for (int i = 0; i < n ; i++) {
+        for (int i = 0; i < n-1 ; i++) {
             for (int j = 0; j < k; j++) {
                 remove = (remove + 1) % (n+1);
                 while (arr[remove] == 1) {
                     remove = (remove + 1) % n;
                 }
             }
-            //System.out.println("remove[" + i + "] = " + remove);
+            System.out.println("remove[" + i + "] = " + remove);
             arr[remove] = 1;
+        }
+
+        for(int i = 1 ; i<= n; i++){
+            if( arr[i] == 0){
+                return  i;
+            }
         }
 
         return  remove ;
