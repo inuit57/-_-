5ee355740063ef7a808f060098b4cf2884aced4b
inuit57 <inuit57@naver.com>
2022-06-04 19:54:45 +0900

    feat : 응급실(우선순위큐 활용)

--- src/test08/Main.java
+++ src/test08/Main.java
@@ -20,27 +20,34 @@ public class Main {
         int m = in.nextInt();
 
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        PriorityQueue<Order> test = new PriorityQueue<>(new Comparator<Order>() {
+
+            @Override
+            public int compare(Order o1, Order o2) {
+                if( o1.danger == o2.danger) {
+                    return o1.num - o2.num;
+                }else{
+                    return o2.danger - o1.danger;
+                }
+            }
+        });
+
         Queue<Order> orderQueue = new LinkedList<>();
         for(int i =0 ; i < n ; i++){
             int danger = in.nextInt() ;
             queue.offer(danger);
             orderQueue.offer(new Order(i,danger));
+            test.offer(new Order(i,danger));
         }
 
-        int k = -1;
-        for(int i=0 ; i < n ;i++){
-            while( queue.peek() != orderQueue.peek().danger){
-                orderQueue.offer(orderQueue.poll());
-            }
-            queue.poll();
-            k = orderQueue.poll().num;
-            if( k == m){
+
+        for(int i =0 ; i < n ; i++){
+            if( test.poll().num == m){
                 System.out.println(i+1);
                 return;
             }
         }
-
-
 
         return ;
     }
