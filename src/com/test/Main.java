a7c75e5fb37c8ec608c98003c6f1d5a53a5c14ca
inuit57 <inuit57@naver.com>
2022-06-06 22:01:15 +0900

    feat : 장난꾸러기

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,27 +1,40 @@
 package com.test;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
+
     public static void main(String[] args){
+        Main T = new Main();
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        Set<Integer> set = new HashSet<>();
+        int[] arr = new int[n];
 
         for(int i =0 ; i < n ; i++){
-            int x= in.nextInt();
-            if( set.contains(x) ){
-                System.out.println("D");
-                return;
-            }
-            set.add(x);
+            arr[i] = in.nextInt();
         }
-        System.out.println("U");
+
+        T.solution(n,arr);
         return ;
     }
 
+    void solution( int n , int[] arr){
+        int pt = -1;
+        for(int i = n-1; i >0 ; i--){
+            if( arr[i] < arr[i-1]){
+                pt = i ;  // 범인 찾음
+                break;
+            }
+        }
+        for(int i =0 ; i < pt ; i++){
+            if( arr[i] > arr[pt]){
+                System.out.print(i+1 + " ");
+                break;
+            }
+        }
+        System.out.print(pt+1);
+    }
 
 
 }
