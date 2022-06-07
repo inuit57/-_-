7a6a300620cb021b770e36c9354b401344dd60dc
inuit57 <inuit57@naver.com>
2022-06-07 13:59:58 +0900

    refactor : 불필요한 코드 정리

--- src/sort/binary/Main.java
+++ src/sort/binary/Main.java
@@ -23,19 +23,21 @@ public class Main {
 
     int solution(int[] arr, int n, int k){
         int answer = Integer.MIN_VALUE ;
-        int max = arr[n-1] ;
+        int max = arr[n-1] ; // - arr[0];
         int min = 1 ;
         // 결정 알고리즘에서는 최소, 최댓값 범위 설정이 중요하다.
 
-        while( min < max){
+//        System.out.println(isPossible(arr,k,2));
+//        //System.out.println(isPossible(arr,k,3));
+        while( min <= max){
             int mid = (min + max)/2 ;
 
             if( isPossible(arr,k,mid)){
-                answer = Math.max(mid,answer); // 현재 최댓값을 저장하자.
+                answer = mid ; // 현재 최댓값을 저장하자.
                 min = mid+1;
             }
             else{
-                max = mid;
+                max = mid-1;
             }
         }
         return answer;
@@ -45,15 +47,15 @@ public class Main {
         int cnt = 1 ;
         for(int i =0 ; i < arr.length - k ; i++){
             for(int j =i+1; j< arr.length ; j++){
-                if((arr[j] - arr[i]) > mid){
-                    //System.out.println("(i , j ) : " + "("+i+","+j+")");
+                if((arr[j] - arr[i]) >= mid){
                     i = j;
                     cnt ++;
                 }
             }
             if( cnt >= k) return true;
-            cnt = 0 ;
+            cnt = 1 ;
         }
-        return (cnt > k) ;
+
+        return (cnt >=k) ;
     }
 }
