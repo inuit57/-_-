b1200afa6d19baf5c5630ddaf021f68cf10ee521
inuit57 <inuit57@naver.com>
2022-06-08 13:41:31 +0900

    feat : DFS - 경로탐색(인접행렬)

---
+++ src/dfs/graph01/Main.java
@@ -0,0 +1,39 @@
+package dfs.graph01;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[][] graph;
+    static int[] chk ;
+    static int answer = 0 ;
+    static  int n ;
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        int m = in.nextInt();
+        graph = new int[n+1][n+1];
+        chk = new int[n+1] ;
+
+        for(int i =0 ; i < m ; i++){
+            graph[in.nextInt()][in.nextInt()] = 1;
+        }
+        chk[1]=1;
+        T.DFS(1) ;
+        System.out.println(answer);
+    }
+
+    private void DFS(int v) {
+        if( v == n ){
+            answer++;
+        }
+        for(int i =0 ; i< n; i++){
+            if(graph[v][i] == 1 && chk[i] ==0){
+                chk[i] =1;
+                DFS(i);
+                chk[i] = 0; // back tracking
+            }
+        }
+    }
+}
