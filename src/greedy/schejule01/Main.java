8d0014adabf2ab18554c72c69566f97c34ade38d
inuit57 <inuit57@naver.com>
2022-06-10 16:37:53 +0900

    fix : 최대수입스케쥴(강사님 풀이)
    - 우선순위큐는 넣어줄 때마다 자동으로 정렬된다는 사실을 활용해서 각날짜에 가능한 값들을 넣어주고 최댓값을 뽑아내는 식으로 구현

--- src/greedy/schejule01/Main.java
+++ src/greedy/schejule01/Main.java
@@ -4,52 +4,65 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+    static int max = Integer.MIN_VALUE;
+    static int n;
+    static class Lector implements Comparable<Lector>{
+        // pQ에는 적용하지않는다.
+        int money;
+        int day;
+
+        public Lector(int money, int day) {
+            this.money = money;
+            this.day = day;
+        }
+
+        @Override
+        public int compareTo(Lector ob) {
+            return ob.day - this.day;
+        }
+    }
+
+    public int solution(ArrayList<Lector> arr){
+        int answer= 0 ;
+        // 기본은 작은 값을 우선순위로 한다. (오름차순)
+        // 값이 들어갈 때마다 자동으로 정렬된다.
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+        Collections.sort(arr); //날짜에 의해서 내림차순 정렬 된다.
+
+        int j= 0 ;
+        for(int i = max ; i >0 ; i--){
+            for( ; j< n ; j++ ){
+                if( arr.get(j).day < i) break;
+                pQ.offer(arr.get(j).money);
+            }
+            if(!pQ.isEmpty()) {
+                answer += pQ.poll();
+            }
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
+        Main T = new Main();
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        n = in.nextInt();
 
-        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
-        int minDay = Integer.MAX_VALUE;
-        int maxDay = Integer.MIN_VALUE;
+        // 제일 마지막 날짜를 따로 저장할 것
+        // 제일 큰 날짜부터 거꾸로 돌면서 처리할 것
+        // 그리고 저장하는 순서는 날짜 기준 내림차순,
+        // 날짜가 같다면 금액 순 내림차순으로 정렬
+        ArrayList<Lector> arr = new ArrayList<>();
+
         for(int i =0 ; i< n; i++){
-            int pay = in.nextInt();
+            int money = in.nextInt();
             int day = in.nextInt();
-            if( !map.containsKey(day)){
-                // 해당 날짜가 없는 경우
-                map.put(day,new ArrayList<>());
-            }
-            map.get(day).add(pay);
-            minDay = Math.min(day,minDay);
-            maxDay = Math.max(day,maxDay);
+            arr.add(new Lector(money,day));
+            // 가장 큰 날짜 계산하기
+            max = Math.max(max,day);
         }
 
-        for(int key : map.keySet()){
-            // 정렬 , 큰 순서대로
-            map.get(key).sort(Collections.reverseOrder());
-        }
+        System.out.println(T.solution(arr));
 
-        // key 들도 정렬하기
-        List<Integer> keyList = new ArrayList<>(map.keySet());
-        Collections.sort(keyList);
-
-        int sum =0 ;
-        for(int d= maxDay ; d >= minDay ; d-- ){  // 반대로 해보자.
-            int max = Integer.MIN_VALUE ;
-            int max_idx = -1 ;
-            for(int key : keyList){
-                if(key < d) continue;
-                if( (!map.get(key).isEmpty()) &&
-                        (max < map.get(key).get(0))){
-                    max_idx = key;
-                    max = map.get(key).get(0);
-                }
-            }
-            if( max_idx >=0 ) {
-                sum += map.get(max_idx).remove(0); // 제일 컸던 놈 제거
-            }
-        }
-
-        System.out.println(sum);
     }
 }
