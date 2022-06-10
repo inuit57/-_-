a2bc84786316ba3cd6a514917654ac383805ae30
inuit57 <inuit57@naver.com>
2022-06-10 23:08:07 +0900

    feat :  조합 및 중복조합 작성

--- src/dfs/combi/Main.java
+++ src/dfs/combi/Main.java
@@ -10,6 +10,8 @@ public class Main {
     static boolean[] chk ;
     static int[] arr ;
 
+    static int[] save;
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
@@ -19,13 +21,29 @@ public class Main {
         chk = new boolean[n];
         arr = new int[n];
 
+        save = new int[r]; // r개를 담을 공간 설정
+
         for(int i=0 ; i<n; i++){
             arr[i] = in.nextInt();
         }
 
-        T.DFS(0,0);
+        //T.DFS(0,0);
+        //T.DFS2(0,0);
+        T.DFS3(0,0);
+    }
 
-
+    private void DFS2(int L, int cnt){
+        if(cnt ==r){
+            for(int x : save){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }else{
+            for(int i =L ; i< n; i++){
+                save[cnt]= i+1;
+                DFS2(i,cnt+1);
+            }
+        }
     }
 
     private void DFS(int L, int cnt) {
@@ -43,6 +61,25 @@ public class Main {
             DFS(L+1,cnt+1); // 선택된 경우
             chk[L] = false; //backtracking
             DFS(L+1,cnt) ; // 선택하지 않은 경우
+        }
+    }
+
+    private void DFS3(int L, int cnt) {
+        // r 개만큼 뽑았다면 출력해주기
+        if(cnt == r){
+            for(int i=0 ; i< n ;i++){
+                if(chk[i]){
+                    System.out.print(i+1 + " ");
+                }
+            }
+            System.out.println();
+            return;
+        }else{
+            for(int i = L ; i< n; i++) {
+                chk[i] = true;
+                DFS3(i+1, cnt + 1); // 선택된 경우
+                chk[i] = false; //backtracking
+            }
         }
     }
 }
