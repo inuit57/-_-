0f09fedc007dde0b4a6d26307f1d456b749e9666
inuit57 <inuit57@naver.com>
2022-06-10 14:30:13 +0900

    feat : 결혼식(강사님 풀이)
    - s,e 로 쪼개는 발상은 생각치도 못했는데
    들어오는 시점, 나가는 시점으로 넣는 발상을 할 필요가 있다.

--- src/dfs/marage/Main.java
+++ src/dfs/marage/Main.java
@@ -1,26 +1,56 @@
 package dfs.marage;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
     static int n ;
 
+    static class Time{
+        int time;
+        char stage;
+
+        public Time(int time, char stage) {
+            this.time = time;
+            this.stage = stage;
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         n = in.nextInt();
-        int[] arr = new int[73] ;
+        ArrayList<Time> arr = new ArrayList<>();
         for(int i=0 ; i< n;i++){
             int s = in.nextInt();
             int e = in.nextInt();
-            for(int j= s; j<e ; j++){
-                arr[j]++;
-            }
+            arr.add(new Time(s,'s'));
+            arr.add(new Time(e,'e'));
         }
 
-        System.out.println(Arrays.stream(arr).max().getAsInt());
+        System.out.println(solution(arr));
+
     }
 
+    private static int solution(ArrayList<Time> arr){
+        arr.sort((o1, o2) -> o1.time == o2.time? o1.stage-o2.stage : o1.time - o2.time);
+
+        int answer = Integer.MIN_VALUE;
+        int cnt =0 ;
+
+        for(Time t : arr){
+            if( t.stage == 's'){
+                cnt ++;
+            }else{
+                cnt --;
+            }
+            answer = Math.max(cnt,answer);
+        }
+
+        return answer;
+    }
 }
