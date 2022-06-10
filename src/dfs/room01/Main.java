700b64f2da6f921ffd04ebffcb9d0e394450f6b3
inuit57 <inuit57@naver.com>
2022-06-10 13:29:51 +0900

    fix : 불필요한 DFS 소스 삭제

--- src/dfs/room01/Main.java
+++ src/dfs/room01/Main.java
@@ -19,8 +19,6 @@ public class Main {
         }
     }
 
-    static boolean[] chk ;
-
     static ArrayList<Room> rooms = new ArrayList<>();
     static int n ;
     static int answer ;
@@ -29,7 +27,6 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         n = in.nextInt();
-        chk = new boolean[n];
         answer = 0;
         for(int i =0; i< n; i++){
             rooms.add(new Room(in.nextInt(),in.nextInt()));
@@ -37,10 +34,6 @@ public class Main {
         // 끝나는 시간으로 오름차순 정렬,
         // 단, 같은 경우에는 시작시간으로 오름차순으로 정렬할 것.
         rooms.sort( (o1,o2)-> o1.end == o2.end ? o1.start - o2.start : o1.end - o2.end);
-//        T.DFS(0,0);
-//        System.out.println(answer);
-        // 10,000건 넘어가니까 stackOverflowError 가 난다.
-        // 조합의 경우의 수가 너무 많아지는 경우에 대해서는 DFS를 사용하지 말 것.
 
         System.out.println(T.solution(rooms,n));
     }
@@ -57,24 +50,5 @@ public class Main {
         return cnt;
     }
 
-    private void DFS(int L, int end) {
-        if(L > n) return;
-        if( L == n){
-            int cnt =0 ;
-            for(int i =0 ; i< n ;i++){
-                if(chk[i]) cnt++ ;
-            }
-            answer = Math.max(answer,cnt);
-        }else{
-            if(!chk[L]){
-                // 넣을 수 있는지 판단이 필요하다.
-                if( end <= rooms.get(L).start){
-                    chk[L] = true;  //넣는 것 가능
-                    DFS(L+1, rooms.get(L).end);
-                    chk[L] = false;
-                }
-                DFS(L+1, end); // 넣지 않는 경우 + 못 넣는 경우
-            }
-        }
-    }
+
 }
