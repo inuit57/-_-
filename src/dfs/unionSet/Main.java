2baf4c7cae4d465c4c88ede95278280a4f2c7af5
inuit57 <inuit57@naver.com>
2022-06-13 10:53:44 +0900

    feat : 친구- Union+find, 강사님 풀이

--- src/dfs/unionSet/Main.java
+++ src/dfs/unionSet/Main.java
@@ -4,59 +4,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
 
-    static boolean[] check;
-    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-    static int find01,find02;
-
-    static boolean flag = false;
-    public static void main(String[] args) {
-        Main T = new Main();
-
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();  // 인원 수
-        check = new boolean[n+1]; // 체크용 배열
-
-        for(int i =0 ; i<=n ; i++){
-            list.add(new ArrayList<>()); // n개의 ArrayList 생성
-        }
-
-        int m = in.nextInt();
-        for(int i=0 ; i< m ; i++){
-            int p1 = in.nextInt();
-            int p2 = in.nextInt();
-
-            list.get(p1).add(p2);
-            list.get(p2).add(p1);
-            // 값 채워넣어주기
-        }
-
-        find01 = in.nextInt();
-        find02 = in.nextInt();
-
-        check[find01] =true;
-
-        T.dfs(find01);
-        if(flag){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
+    static int[] unf;
+    public static int find(int v){
+        if(v == unf[v] ) return v;
+        else return unf[v] = find(unf[v]);
     }
 
-    private void dfs(int L) {
-        if( list.get(L).isEmpty()) return ;
-        else {
-            if (list.get(L).contains(find02)) {
-                flag = true;
-                return;
-            }
-            for (int x : list.get(L)) {
-                if( !check[x]) {
-                    check[x] = true;
-                    dfs(x);
-                    check[x] = false;
-                }
-            }
+    public static void union(int a, int b){
+        int fa = find(a);
+        int fb = find(b);
+        if( fa != fb) unf[fa] = fb;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        unf = new int[n+1] ;
+        for(int i=1; i<= n; i++) unf[i] = i;
+        for(int i=1; i<= m; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            union(a,b);
         }
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int fa = find(a);
     }
 }
