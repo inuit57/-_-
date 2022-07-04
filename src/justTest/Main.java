c6c6e0e67f9f90d1397e8f234611786cc6c9d40d
inuit57 <inuit57@naver.com>
2022-07-04 17:12:07 +0900

    feat : 백준 11726 : 2x1 타일, 피보나치 수열

--- src/justTest/Main.java
+++ src/justTest/Main.java
@@ -3,15 +3,25 @@ package justTest;
 import java.util.Scanner;
 
 public class Main {
+
+    static int[] arr = new int[1001] ;
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
-        String input = in.next();
+        int input = in.nextInt();
+        System.out.println(fibo(input));
+    }
 
-        String[] splits = input.split("_" ,3);
-        for (String split : splits) {
-            if( split == null) System.out.println("!!!");
-            System.out.println("1 : " + split);
+    public static int fibo(int n){
+        if( arr[n] != 0) return arr[n] ;
+        if( n == 0 || n == 1) {
+            return arr[n] = 1 ;
+        }else{
+            if( arr[n-1] == 0) arr[n-1] = fibo(n-1) %10007;
+            if( arr[n-2] == 0) arr[n-2] = fibo(n-2) %10007;
+
+            return arr[n] = (arr[n-1] + arr[n-2])%10007 ;
         }
     }
 
