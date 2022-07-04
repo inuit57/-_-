fbd33e5ac77798897e9dfc14e25e0d1b4ca7aace
inuit57 <inuit57@naver.com>
2022-07-04 17:16:52 +0900

    feat : 백준 11726 : 2x1 타일, 반복문으로 수정

--- src/justTest/Main.java
+++ src/justTest/Main.java
@@ -10,19 +10,27 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         int input = in.nextInt();
-        System.out.println(fibo(input));
-    }
+//        System.out.println(fibo(input));
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
 
-    public static int fibo(int n){
-        if( arr[n] != 0) return arr[n] ;
-        if( n == 0 || n == 1) {
-            return arr[n] = 1 ;
-        }else{
-            if( arr[n-1] == 0) arr[n-1] = fibo(n-1) %10007;
-            if( arr[n-2] == 0) arr[n-2] = fibo(n-2) %10007;
-
-            return arr[n] = (arr[n-1] + arr[n-2])%10007 ;
+        for(int i = 3; i<= 1000 ; i++){
+            arr[i] = ( arr[i-2] + arr[i-1] ) % 10007 ;
         }
+        System.out.println(arr[input]);
     }
+
+//    public static int fibo(int n){
+//        if( arr[n] != 0) return arr[n] ;
+//        if( n == 0 || n == 1) {
+//            return arr[n] = 1 ;
+//        }else{
+//            if( arr[n-1] == 0) arr[n-1] = fibo(n-1) %10007;
+//            if( arr[n-2] == 0) arr[n-2] = fibo(n-2) %10007;
+//
+//            return arr[n] = (arr[n-1] + arr[n-2])%10007 ;
+//        }
+//    }
 
 }
