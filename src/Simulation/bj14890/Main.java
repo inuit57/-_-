62a29c46cfa27739cce448c849e00480413a6595
inuit57 <inuit57@naver.com>
2022-07-15 11:29:45 +0900

    doc : 백준 14890 경사로 실패... 내리막 처리가 어렵네

--- src/Simulation/bj14890/Main.java
+++ src/Simulation/bj14890/Main.java
@@ -9,6 +9,29 @@ public class Main {
     // 그게 가장 확실하게 체크하는 방식이 되지 않을까 싶네요.
 
     // 한 줄씩 체크하도록 하고, visited[] 1차원 배열을 놓아서 처리하는 게 좋을 듯 합니다.
+    // 길이가 1일 때, 되게 체크하기가 애매한 부분들이 많네요. visit 배열로 처리를 좀 돕는 편이 좋아보입니다.
+    /*
+
+    3 1
+6 6 6
+5 5 6
+5 6 6
+    답 : 5
+    내 코드 : 6
+-> 이거 테스트 필요.
+
+
+6 1
+5 3 3 4 6 6
+5 4 3 4 6 6
+4 3 3 4 5 5
+3 3 3 4 4 4
+3 3 3 3 4 4
+5 3 3 4 5 5
+    답 : 7
+    내 코드 : 8
+
+     */
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -19,6 +42,7 @@ public class Main {
         // 값 저장
         int[][] map = new int[N][N];
         makeMap(in, N, map);
+
 
         int answer= 0;
         for(int i =0 ; i< N ; i++){
@@ -41,78 +65,67 @@ public class Main {
                         row_flag = false;
                         break;
                     }else if( h < 0) {   // 오르막을 만나는 경우
-                        if (row_cnt >= L-1 ) {
-                            row_cnt -= L ;
+                        if (row_cnt >= L ) {
+                            row_cnt = 1 ;
                         } else {
                             row_flag = false;
                             break;
                         }
                     }else{  // 내리막을 만나는 경우
-                        row_cnt = 1;
-                        j++; // 한칸 앞으로 증가.
-                        for(; j< N; j++){
-                            if( map[i][j-1] == map[i][j]){
-                                row_cnt ++;
+                        for(int k=1 ; k <= L ; k++){
+                            if( (j+k < N) &&  map[i][j+k-1] == map[i][j+k]){
+                                continue;
                             }else {
+                                row_flag = false;
+                                j=N;
                                 break;
                             }
                         }
-                        if(row_cnt < L ){
-                            row_flag = false;
-                            break;
-                        }else{
-                            row_cnt -= L ;
-                            j--;
-                        }
+                        j+=L ;
                     }
                 }
             }
             if( row_flag ){
                 answer ++;
-//                System.out.println("map["+i+"][?]");
-//                System.out.println(map[i][0]);
+                System.out.println("map["+i+"][?]");
+                System.out.println(map[i][0]);
             }
 
             for(int j=1; j< N ; j++){
+                if( i == 4 ){
+                    System.out.println("map[j][4] :" + map[j][4]);
+                }
                 int h = map[j-1][i] - map[j][i] ;
                 if( h == 0 ){
                     col_cnt ++;
                     // 바로 전의 항목과 같다면 +1
                 }else{
-                    if( Math.abs( h ) > 1){
+                    if( Math.abs( h ) > 1 ){
                         col_flag = false;
                         break;
                     }else if( h < 0) {  // 오르막
-                        if (col_cnt >= L-1 ) {
-                            col_cnt -= L  ;
+                        if (col_cnt >= L) {
+                            col_cnt = 1 ;
                         } else {
-                            col_flag = false;
                             break;
                         }
-                    }else{  // 내리막 
-                        col_cnt = 1;
-                        j++; // 한칸 앞으로 증가.
-                        for(; j< N; j++){
-                            if( map[j-1][i] == map[j][i] ){
-                                col_cnt ++;
+                    }else{  // 내리막
+
+                        for(int k=1 ; k <= L ; k++){
+                            if( (j+k < N) &&  map[j+k-1][i] == map[j+k][i]){
+                                continue;
                             }else {
                                 break;
                             }
                         }
-                        if(col_cnt < L ){
-                            col_flag = false;
-                            break;
-                        }else{
-                            col_cnt -= L ;
-                            j--;
-                        }
+                        j+=L ;
                     }
                 }
             }
             if( col_flag ){
                 answer ++;
-//                System.out.println("map[?]["+i+"]");
-//                System.out.println(map[0][i]);
+                System.out.println("map[?]["+i+"]");
+                System.out.println(map[0][i]);
             }
         }
 
