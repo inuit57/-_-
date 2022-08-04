e1cbeccd791292c5ab175f21b9877a1dd28b2c7c
inuit57 <inuit57@naver.com>
2022-08-04 10:37:40 +0900

    feat : 백준, 숨바꼭질4 (BFS) 시도
    이동경로를 찍어줘야 하는 건데, 생각대로 잘 되진 않네요.

--- src/bfs/bj/숨바꼭질1/Main.java
+++ src/bfs/bj/숨바꼭질1/Main.java
@@ -1,8 +1,7 @@
 package bfs.bj.숨바꼭질1;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,38 +12,62 @@ public class Main {
         int start = in.nextInt();
         int obj = in.nextInt();
 
-        System.out.println(T.solution(start,obj));
-
+        //System.out.println(T.solution(start,obj));
+        T.solution(start,obj);
     }
 
     private int solution(int start, int obj) {
-        boolean[] visit = new boolean[100001];
+//        boolean[] visit = new boolean[100001];
+        int[] visit = new int[100001];
         Queue<Integer> queue = new LinkedList<>();
 
         queue.add(start);
-        visit[start] = true;
+        visit[start] = start;
 
         int answer = 0;
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0 ; i< size ; i++){
                 int curr = queue.poll();
-                if( curr == obj ) return  answer;
+                if( curr == obj ){
+                    StringBuilder sb = new StringBuilder();
+
+                    ArrayList<Integer> arr = new ArrayList<>();
+                    int before = visit[obj];
+                    arr.add(obj);
+                    arr.add(before);
+                    while( visit[before] != start){
+                        before = visit[before];
+                        arr.add(before);
+                    }
+                    arr.add(start);
+
+                    Collections.reverse(arr);
+                    for(int a : arr){
+                        sb.append(a);
+                        sb.append(" ");
+                    }
+
+                    System.out.println(answer);
+                    System.out.println(sb);
+
+                    return answer;
+                }
 
                 int next = curr-1;
-                if( next >= 0 &&  !visit[next] ){
+                if( next >= 0 &&  visit[next] == 0  ){
                     queue.add(next);
-                    visit[next] = true;
+                    visit[next] = curr;
                 }
                 next = curr +1;
-                if( next <= 100000 &&  !visit[next] ){
+                if( next <= 100000 && visit[next] == 0 ){
                     queue.add(next);
-                    visit[next] = true;
+                    visit[next] = curr;
                 }
                 next = curr*2 ;
-                if( next <= 100000 &&  !visit[next] ){
+                if( next <= 100000 &&  visit[next] == 0 ){
                     queue.add(next);
-                    visit[next] = true;
+                    visit[next] = curr;
                 }
             }
             answer++;
