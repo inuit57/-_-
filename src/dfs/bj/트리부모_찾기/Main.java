9a2712fed8514c1dade410d8df6fbfce24740a04
inuit57 <inuit57@naver.com>
2022-08-05 15:14:24 +0900

    docs : 트리 부모 찾기 복귀

--- src/dfs/bj/트리부모_찾기/Main.java
+++ src/dfs/bj/트리부모_찾기/Main.java
@@ -12,6 +12,9 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int i, j;
 
+        // 입력의 크기가 큰 경우에는 NxN 배열을 선언할 경우
+        // 메모리가 터질 수 있으니까 인접리스트로 접근하는 것이 좋다.
+        
         // 연결 정보
         ArrayList<Integer>[] list = new ArrayList[n+1];
         // 초기화
@@ -30,6 +33,9 @@ public class Main {
 
         // 결과 값(부모값)
         // 예: parents[i] = j (i의 부모 : j)
+
+        // 맵에 저장하는 것을 생각했는데 그냥 1차원 배열에 저장하였더라도
+        // 크게 문제가 없었을 듯 싶다. 둘다 어짜피 int 값이기도 하고.
         int[] parents = new int[n+1];
 
         // dfs
