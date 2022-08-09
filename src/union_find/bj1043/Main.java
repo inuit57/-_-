5391ceaaedc17d67fff6ee5bf3385e94bd20a792
inuit57 <inuit57@naver.com>
2022-08-09 11:19:17 +0900

    feat : 백준 1043, 세번째 시도 (union find, 조정) - 성공

--- src/union_find/bj1043/Main.java
+++ src/union_find/bj1043/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        Main T = new Main();
+
 
         n = in.nextInt();
         m = in.nextInt();
@@ -34,56 +34,61 @@ public class Main {
             unf[i] = i;
         }
 
+        int answer = m;
         int t = in.nextInt();
-        if( t == 0){
-            System.out.println(m);
-            return;
-        }
 
-        // 초기 아는 놈들 설정하기
-        int before = in.nextInt();
-        unf[before] = 0 ;
-        for(int i=1; i< t; i++){
-            int next = in.nextInt();
-            union(before, next);
-            before = next;
-        }
+        int xman ;
 
-        ArrayList<ArrayList<Integer>> inputs = new ArrayList<>();
-
-        boolean flag = false ;
-        for(int i=0; i< m ;i++){
-            int gn = in.nextInt(); // 그룹 인원 숫자
-
-            ArrayList<Integer> arr = new ArrayList<>();
-            before = in.nextInt();
-            arr.add(before);
-
-            for(int j=1; j< gn ; j++){
+        if( t != 0 ) {
+            // 초기 아는 놈들 설정하기
+            int before = in.nextInt();
+            for (int i = 1; i < t; i++) {
                 int next = in.nextInt();
-                arr.add(next);
-                union(before,next);
-                next = before;
-                // 같은 그룹에 속한 것들은 union 으로 묶어주자
+                union(before, next);
+                before = next;
+            }
+            xman = before; // 나중에 확인 용도
+
+            ArrayList<ArrayList<Integer>> inputs = new ArrayList<>();
+
+            for (int i = 0; i < m; i++) {
+                int gn = in.nextInt(); // 그룹 인원 숫자
+
+                ArrayList<Integer> arr = new ArrayList<>();
+                before = in.nextInt();
+                arr.add(before);
+
+                for (int j = 1; j < gn; j++) {
+                    int next = in.nextInt();
+                    arr.add(next);
+                    union(before, next);
+                    before = next;
+                    // 같은 그룹에 속한 것들은 union 으로 묶어주자
+                }
+
+                inputs.add(arr);
             }
 
-            inputs.add(arr);
-            flag = false;
-        }
+//            printUF();
 
-        // 허풍 떨지 정하기
-        int answer = inputs.size();
-        for(ArrayList<Integer> numbers : inputs){
-            for(int num : numbers){
-                if( find(num) != num ){
-                    answer --;
-                    break;
+            // 허풍 떨지 정하기
+            for (ArrayList<Integer> numbers : inputs) {
+                for (int num : numbers) {
+                    if (find(num) == find(xman)) {
+                        answer--;
+                        break;
+                    }
                 }
             }
         }
-
         System.out.println(answer);
 
+    }
+
+    private static void printUF() {
+        for(int i = 1; i< n+1; i++){
+            System.out.println(i + " : " + find(i));
+        }
     }
 
 }
