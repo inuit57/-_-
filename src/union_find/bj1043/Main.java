494edf24e2411ace9f64d840510e17c56a512684
inuit57 <inuit57@naver.com>
2022-08-09 11:11:42 +0900

    feat : 백준 1043, 두번째 시도 (union find로 변경) - 실패

--- src/union_find/bj1043/Main.java
+++ src/union_find/bj1043/Main.java
@@ -6,12 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     static int n,m;
+
+    static int[] unf ;
+
+    static int find(int v){
+        if( unf[v] == v) return v;
+        else return unf[v] = find(unf[v]);
+    }
+
+    static void union(int a,int b){
+        int x = find(a);
+        int y = find(b);
+        if( x != y ){
+            unf[x] = y;
+        }
+    }
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         Main T = new Main();
 
         n = in.nextInt();
         m = in.nextInt();
+        unf = new int[n+1] ;
+
+        for(int i=1 ; i< n+1; i++){
+            unf[i] = i;
+        }
 
         int t = in.nextInt();
         if( t == 0){
@@ -19,39 +40,42 @@ public class Main {
             return;
         }
 
-        boolean[] peoples = new boolean[n+1];
-        for(int i=0; i< t; i++){
-            peoples[in.nextInt()] = true;
+        // 초기 아는 놈들 설정하기
+        int before = in.nextInt();
+        unf[before] = 0 ;
+        for(int i=1; i< t; i++){
+            int next = in.nextInt();
+            union(before, next);
+            before = next;
         }
 
         ArrayList<ArrayList<Integer>> inputs = new ArrayList<>();
 
+        boolean flag = false ;
         for(int i=0; i< m ;i++){
-            int gn = in.nextInt();
+            int gn = in.nextInt(); // 그룹 인원 숫자
 
-            boolean flag = false;
             ArrayList<Integer> arr = new ArrayList<>();
-            for(int j=0; j< gn ; j++){
+            before = in.nextInt();
+            arr.add(before);
+
+            for(int j=1; j< gn ; j++){
                 int next = in.nextInt();
-                if( peoples[next] ) flag = true;
                 arr.add(next);
+                union(before,next);
+                next = before;
+                // 같은 그룹에 속한 것들은 union 으로 묶어주자
             }
-            if( flag ){
-                // 진실을 알고 있는 놈들도 다 때려넣어주자
-                for(int ab : arr){
-                    peoples[ab] = true;
-                }
-            }else {
-                // 해당되지 않는 것만 집어넣자
-                inputs.add(arr);
-            }
+
+            inputs.add(arr);
             flag = false;
         }
 
+        // 허풍 떨지 정하기
         int answer = inputs.size();
         for(ArrayList<Integer> numbers : inputs){
             for(int num : numbers){
-                if( peoples[num] ){
+                if( find(num) != num ){
                     answer --;
                     break;
                 }
