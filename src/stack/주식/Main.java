d741d7562a77df15962dc2f7953edd1cc66afc51
inuit57 <inuit57@naver.com>
2022-11-02 10:16:25 +0900

    feat : 프로그래머스, 주식가격(스택)

---
+++ src/stack/주식/Main.java
@@ -0,0 +1,51 @@
+package stack.주식;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        int[] prices = {1, 2, 3, 2, 3};
+
+        int[] answer = m.solution(prices);
+        StringBuilder sb = new StringBuilder();
+        for(int a : answer){
+            sb.append(a).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        // 좌표 값을 저장해보자
+        Stack<Integer> stk = new Stack<>();
+
+        // 1, 2, 3
+        // 1, 2, 3, "2"
+        // (0, 1, 2) i=3
+        // answer = { 0, 0 , 1 }  // 3 - 2(top) , 조건은 top > prices[i] 다음 것보다 top 이 클 때
+
+        // (0,1,3) 1,2,2 인 상태
+        // (0,1,3,4) 1,2,2,3 끝.
+
+        // 남은 것들은 이제 길이(4)에서 빼주면 될 듯
+        // (4,3,1,1,0)
+        for(int i =0 ; i < prices.length; i++){
+            while(!stk.isEmpty() && prices[stk.peek()] > prices[i]){
+                int top = stk.peek();
+                stk.pop();
+                answer[top] = (i - top);
+            }
+            stk.push(i);
+        }
+
+        for(int i=0; i< prices.length ; i++){
+            if( answer[i] == 0 ) answer[i] = prices.length - i - 1;
+        }
+
+        return answer;
+    }
+}
