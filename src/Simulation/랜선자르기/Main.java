1bda5f24a3a1d2c38b8b86937a8ec1c580c3941e
inuit57 <inuit57@naver.com>
2022-11-07 13:50:13 +0900

    feat : 랜선자르기, 이분 탐색

--- src/Simulation/랜선자르기/Main.java
+++ src/Simulation/랜선자르기/Main.java
@@ -1,55 +1,73 @@
 package Simulation.랜선자르기;
 
+import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
 
+    /**
+     * 예외 테스트 케이스
+     * 4 1
+     * 802
+     * 743
+     * 457
+     * 539
+     */
     static int[] arr ;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s =bf.readLine();
-        StringTokenizer st = new StringTokenizer(s);
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        String s =bf.readLine();
+//        StringTokenizer st = new StringTokenizer(s);
+//        int k = Integer.parseInt(st.nextToken());
+//        int n = Integer.parseInt(st.nextToken());
+
+        // 스캐너 입력 테스트
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        int n = in.nextInt();
 
         Main T = new Main();
 
         // 입력 받기
-        long avg = 0 ;
+        long max = Integer.MIN_VALUE ;
         arr = new int[k] ;
         for(int i=0; i< k; i++){
-            int tmp = Integer.parseInt(bf.readLine()) ;
+            //int tmp = Integer.parseInt(bf.readLine()) ;
+            int tmp = in.nextInt();
             arr[i] = tmp;
-            avg += tmp ;
+            max = Math.max(max, tmp);
         }
 
-        avg /= k ; // 평균값으로 시도
-        System.out.println(T.solution(avg,n) );
+        long answer  = T.solution(max,n);
+        System.out.println(answer );
     }
 
-    public long solution(long avg, int n  ){
-        long stt , end , cnt = 0   ;
-        stt = 0 ;
-        end = avg ;
+    public long solution(long max, int n  ){
+        long answer = 0;
 
+        long stt , end , cnt   ;
+        stt = 0 ;
+        end = max ;
+        long mid = max ; // 1개만 뽑아내는 예외케이슷 고려, 최댓값으로 첫시도하기
         while( stt < end ){
-            long mid = (stt+end)/2 ;
             cnt = getCount(mid);
             if( cnt < n ){
-                end = mid-1 ;
+                end = mid ;
             }else{
-                stt = mid ;
+                answer = Math.max(mid, answer);
+                stt = mid +1;
             }
+            mid = (stt+end)/2 ;
         }
-        return stt;
+
+        return answer;
     }
+
 
 
     public int getCount(long len ){
